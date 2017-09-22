fc07af674538b5fb204c76c867f11835b6456318
Prabhash Mohanty <pmohanty@tavisca.com>
2017-09-22 17:44:09 +0530

    Added search file using lucene

---
+++ SearchIndexer/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SearchIndexer;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //SampleDataFileReader reader = new SampleDataFileReader();
+            //IEnumerable<SampleDataFileRow> rows= reader.ReadAllRows();
+            SampleData data1 = new SampleData("Sun Shine","Hotel","Bahamas");
+            SampleData data2 = new SampleData("Sun Flower", "Hotel", "Miami");
+            SampleData data3 = new SampleData("Sun and Love", "Hotel", "Florida");
+            List<SampleData> rows = new List<SampleData>();
+            rows.Add(data1);
+            rows.Add(data2);
+            rows.Add(data3);
+            LuceneService service = new LuceneService();
+            service.BuildIndex(rows);
+            rows=service.Search("Su");
+        }
+    }
+}
